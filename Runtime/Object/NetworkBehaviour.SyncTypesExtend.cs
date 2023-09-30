093c033c7dc5ad2f70aa3b456aa971128ed1b7f6
DuyCa <ca@wolffungame.com>
2023-09-30 11:43:08 +0700

    f

---
+++ Runtime/Object/NetworkBehaviour.SyncTypesExtend.cs
@@ -0,0 +1,13 @@
+﻿using System.Buffers;
+
+namespace FishNet.Object
+{
+    public abstract partial class NetworkBehaviour
+    {
+        private void OnDestroy()
+        {
+            foreach (var syncType in _syncTypeWriters)
+                syncType.Dispose();
+        }
+    }
+}
